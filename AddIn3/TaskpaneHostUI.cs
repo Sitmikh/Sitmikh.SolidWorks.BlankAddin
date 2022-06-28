d8ce9c877813b7a97af0be197ed804b7a7183fa5
Sitmikh <misha.sitnikov.97@mail.ru>
2022-06-28 23:02:17 +0700

    Исправлены названия столбцов

--- AddIn3/TaskpaneHostUI.cs
+++ AddIn3/TaskpaneHostUI.cs
@@ -225,12 +225,14 @@ namespace Sitmikh.SolidWorks.BlankAddin
 
             dataGridView1.Rows.RemoveAt(0);
             dataGridView1.Rows.RemoveAt(0);
-            dataGridView1.Columns[0].HeaderText = "d"; //оформляем столбцы
+            dataGridView1.Columns[0].HeaderText = "Наименование"; //оформляем столбцы
             dataGridView1.Columns[0].Width = 50;
-            dataGridView1.Columns[1].HeaderText = "D";
+            dataGridView1.Columns[1].HeaderText = "";
             dataGridView1.Columns[1].Width = 50;
-            dataGridView1.Columns[2].HeaderText = "L";
+            dataGridView1.Columns[2].HeaderText = "D";
             dataGridView1.Columns[2].Width = 50;
+            dataGridView1.Columns[3].HeaderText = "L";
+            dataGridView1.Columns[3].Width = 50;
             dataGridView1.Columns[3].HeaderText = "Tкр";
             dataGridView1.Columns[3].Width = 500;
 
